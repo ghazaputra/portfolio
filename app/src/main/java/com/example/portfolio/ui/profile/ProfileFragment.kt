ba68cfc8cdbb2ec0d0f2bfaf1f9bd0ec3c9e6175
GhazaPutra <ghazaputras@gmail.com>
2021-11-17 13:58:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/portfolio/ui/profile/ProfileFragment.kt
@@ -0,0 +1,28 @@
+package com.example.portfolio.ui.profile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.portfolio.R
+import com.example.portfolio.databinding.FragmentProfileBinding
+
+
+class ProfileFragment : Fragment() {
+
+    private var _binding: FragmentProfileBinding? = null
+
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentProfileBinding.inflate(inflater,container,false)
+        return binding.root
+
+
+    }
+
+
+}
